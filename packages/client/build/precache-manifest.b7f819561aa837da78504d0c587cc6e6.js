195fb5127bb357c526435ab228e5a51cd0a8e8b8
lxgreen <alxgreen@gmail.com>
2020-10-10 13:46:28 +0300

    lerna+workspaces init setup

---
+++ packages/client/build/precache-manifest.b7f819561aa837da78504d0c587cc6e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8206c93153d29c44ca428d3af7be6f2c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74cc4904ab8e39cb2f06",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "372980b524596e331c10",
+    "url": "/static/js/2.bf58c484.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bf58c484.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74cc4904ab8e39cb2f06",
+    "url": "/static/js/main.0a479989.chunk.js"
+  },
+  {
+    "revision": "9bc38596806b2e761e55",
+    "url": "/static/js/runtime-main.a5473a2d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
